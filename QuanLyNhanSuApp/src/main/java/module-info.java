84d0876a179abe09e69401dac82a4eac3ccbb367
LaGiaLoc2004 <lagialoc2004@gmail.com>
2025-10-10 08:35:08 +0700

    thêm lệnh cần file sql

--- QuanLyNhanSuApp/src/main/java/module-info.java
+++ QuanLyNhanSuApp/src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module ueh.quanlynhansuapp {
     requires javafx.controls;
     requires javafx.fxml;
-
+        requires java.sql;
     opens ueh.quanlynhansuapp to javafx.fxml;
     exports ueh.quanlynhansuapp;
 }
