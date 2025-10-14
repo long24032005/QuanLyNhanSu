c85240a70c000ec679a46d34dcbcd35da684dbc2
lagia <lagia@LAQ>
2025-10-14 18:53:16 +0700

    thêm class main

---
+++ QuanLyNhanSuApp/src/main/java/ueh/quanlynhansuapp/main.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ueh.quanlynhansuapp;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author lagia
+ */
+public class main {
+    @FXML
+    Button main_btnhansu;
+    @FXML
+    Button main_btphongban;
+    
+    @FXML
+    private void main_nhansuAction() throws IOException {
+        canhbao.thongbao("Thông báo", "bạn đang vào bảng nhân sự");
+        App.setRoot("nhansu");
+    } 
+    @FXML
+    private void main_phongbanAction() throws IOException {
+        canhbao.thongbao("Thông báo", "bạn đang vào bảng phòng ban");
+        App.setRoot("phongban");
+    } 
+}
