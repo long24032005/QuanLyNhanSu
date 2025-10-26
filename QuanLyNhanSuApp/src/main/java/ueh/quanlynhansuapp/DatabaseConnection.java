b4a7c79e1f3923d87f0adfb2d8c8b7a3e8973da0
lagis <172578465+LaGiaLoc2004@users.noreply.github.com>
2025-10-26 17:16:12 +0700

    đổi giao diện main - mở ra cho quản lý

--- QuanLyNhanSuApp/src/main/java/ueh/quanlynhansuapp/DatabaseConnection.java
+++ QuanLyNhanSuApp/src/main/java/ueh/quanlynhansuapp/DatabaseConnection.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
 
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/quanlynhansu_db?useSSL=false&serverTimezone=UTC";
+    private static final String DB_URL = "jdbc:mysql://localhost:3306/quanlynhansu_db?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
     private static final String USER = "root";
     private static final String PASS = "Nam@2005"; 
 
